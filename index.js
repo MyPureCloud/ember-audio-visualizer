c950b89332b140fda637b388421df75945353ec4
Garrett Jensen <garrett.jensen@inin.com>
2016-12-02 10:39:54 -0500

    change isDeveloping

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 module.exports = {
   name: 'ember-audio-visualizer',
   isDevelopingAddon: function () {
-    return this.app && this.app.env === 'development';
+    // return this.app && this.app.env === 'development';
+    return Boolean(process.env.EAV_DEV_MODE);
   },
   included: function (app) {
     this._super.included(app);
